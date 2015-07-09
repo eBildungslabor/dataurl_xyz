b5517a13ed78733004832d0b217b537535990cee
Sean Behan <inbox@seanbehan.com>
2015-07-09 16:06:03 -0400

    init

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template as render, request, make_response, redirect
+from base64 import urlsafe_b64encode as encode, urlsafe_b64decode as decode, b64encode, standard_b64encode
+from bz2 import compress, decompress
+import marshal
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render("home.html")
+
+@app.route("/pack", methods=["POST"])
+def pack():
+    content_type = request.form.get('content-type', 'text/plain')
+    body = request.form.get('body', '')
+
+    payload = marshal.dumps(dict(
+        body=body,
+        content_type=content_type
+    ))
+    payload = encode(compress(payload))
+
+    return redirect("/%s" % payload)
+
+@app.route("/<data>")
+def unpack(data):
+    data = marshal.loads(decompress(decode(str(data))))
+    resp = make_response(data['body'])
+    resp.headers['content-type'] = data['content_type']
+
+    return resp
+
+if __name__=='__main__':
+    app.run(debug=True)
